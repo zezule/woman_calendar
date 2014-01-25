e10051ab29491224e0dc14c9b969d02443d02f06
Izabela Zimna - www-data <izabella.zimna@gmail.com>
2014-01-25 23:15:06 +0000

    test - nauka git v 3

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -2,8 +2,23 @@
 
 class Main extends CI_Controller {
 
+	public function message($to = 'World')
+	{ //php index.php main message -- CLI
+		if (true === $this->input->is_cli_request()){
+			echo "Hello {$to}!".PHP_EOL;
+		}
+	}
+	
 	public function index()
 	{
+	  $sections = array(
+			'config'  => TRUE,
+			'queries' => TRUE
+    );
+
+		$this->output->set_profiler_sections($sections);
+		$this->output->enable_profiler(true);
+		
 		if ( ! file_exists('application/views/main_page/index.php')){
 			show_404();
 		}
